aa9f80850bf8ad232e1b423fe2c4a934d868c005
John-Anosike <john.anosike2819@gmail.com>
2025-11-30 14:26:02 -0500

    Minor change to app.py
    Added a basic Gradio interface that allows us to take a randomized list and sort it. Visually, all that's shown is the unsorted and sorted list. 
    
    Next step is making the list customizable.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import random
 
-
+# merges two lists together, out-of-place
 def m_merge(list_a, list_b):
     i, j = 0, 0
     new_list = []
@@ -23,6 +23,8 @@ def m_merge(list_a, list_b):
     return new_list
 
 
+# recursive portion of the mergesort function
+# TO-DO: Create some visual demonstration of the list being split and sorted
 def m_sort(m_list):
     # if the list can no longer be split in two, return it
     # otherwise, recursively return the merge between two sorted lists
@@ -39,8 +41,15 @@ def random_list(length, limit):
         new_list.append(random.randint(1, limit))
     return new_list
 
+# basic gradio interface, just for testing purposes
+interface = gr.Interface(
+    fn = m_sort,  # interface works off of our mergesort function
+    inputs=[
+        gr.List(datatype="number", 
+                label="Unsorted List", 
+                value=random_list(10, 100)) # TO-DO: change the value to a customizable list, input by the user
+    ],
+    outputs=[gr.List(datatype="number", label="Sorted List")],
+)
 
-print("Creating list...")
-temp_list = random_list(20, 10)
-print(f"List successfully created! Now sorting {temp_list}")
-print(m_sort(temp_list))
+interface.launch()
