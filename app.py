b07c00e56541739943efdc1f39f187f6b8d3e49b
John-Anosike <john.anosike2819@gmail.com>
2025-12-07 22:45:22 -0500

    Major update to app.py
    Major changes include:
    - Adding a slider-based system that allows the user to see each step of the merging process (could possibly show the sorting process independently)
    - Layout changes
    
    Minor changes include:
    - Changing the theme of the layout
    
    TO-DO: try to have steps for both merge and sort, as well as providing clearer explanations for them

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import gradio as gr
 import random
 
+steps = []
+
 # merges two lists together, out-of-place
 def m_merge(list_a, list_b):
     i, j = 0, 0
@@ -31,36 +33,59 @@ def m_sort(m_list):
     if len(m_list) < 2:
         return m_list
     else:
-        return m_merge(m_sort(m_list[:len(m_list)//2]), m_sort(m_list[len(m_list)//2:]))
+        merged = m_merge(m_sort(m_list[:len(m_list)//2]), m_sort(m_list[len(m_list)//2:]))
+        steps.append(merged)
+        return merged
 
 
 # generates list of random integers from 1-100 of user-defined length
 def generate_list(length):
     g_list = []
+    steps.clear()
     if int(length) < 0:
         return "ERROR: List length cannot be negative!"
     else:
         for n in range(int(length)):
             g_list.append(random.randint(1, 100))
-        return g_list
-    
+        return g_list, gr.update(maximum=len(g_list))
+
+
+def view_steps(val):
+    output = str(steps[val])
+    output = output.replace("[", "")
+    output = output.replace("]", "")
+    return output, gr.update(visible=True)
+
 
 # gradio blocks interface
-with gr.Blocks() as instance:
-    # section for creating random list
-    with gr.Column():
-        inp = gr.Textbox(placeholder="Enter a positive number for the length of the list", label="List Length")
-        create_btn = gr.Button("Create List")
-    
-    # section for sorting list
+with gr.Blocks(title="MergeSort Demonstration") as instance:
+    # section for creating random list and sorting said list
     with gr.Row():
         with gr.Column():
-            out_list = gr.List(label="Generated List")
-            create_btn.click(fn=generate_list, inputs=inp, outputs=out_list)
+            inp = gr.Textbox(placeholder="Enter a positive number for the length of the list", label="List Length")
         with gr.Column():
+            create_btn = gr.Button("Create List")
             sort_btn = gr.Button("Sort List")
-            sorted_list = gr.Textbox(label="Sorted List")
-            sort_btn.click(fn=m_sort, inputs=out_list, outputs=sorted_list)
 
-instance.launch()
+    with gr.Row():
+        with gr.Column():
+            out_list = gr.List(headers=["Generated List"])
+        with gr.Column():
+            sorted_list = gr.List(headers=["Sorted List"])
+
+    # section for viewing steps
+    with gr.Row():
+        # all defaulted to False; they get enabled after the list is sorted and after a step is viewed
+        step_slider = gr.Slider(minimum=0, maximum=1, step=1, label="Current Step")
+        steps_btn = gr.Button("View Step")
+    step = gr.Label(label="Recursive Step: merge the partitions and sort them from least to greatest", visible=False)
+
+    create_btn.click(fn=generate_list, inputs=inp, outputs=[out_list, step_slider])
+    sort_btn.click(fn=m_sort, inputs=out_list, outputs=sorted_list)
+    steps_btn.click(fn=view_steps,
+                    inputs=step_slider,
+                    outputs=[step, step])
+
+# gr.themes.builder()
+instance.launch(theme=gr.themes.Citrus())
 
